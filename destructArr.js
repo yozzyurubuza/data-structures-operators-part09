521616fbd078fcc8014b2fb41cb3a326dfa4dd26
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-20 03:09:13 +0800

    Added codes about Destructuring Objects and Separated Files per Lecture

--- destructArr.js
+++ destructArr.js
@@ -45,3 +45,44 @@ const restaurant = {
     );
   },
 };
+
+//Retrieving Array in a classic way
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//New way to retrieve - Destructuring
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+//Can retrieve array even if not complete, just 1st and 2nd element
+//Just leave a space in between to retrieve other array place
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//Usual way of Switching without destructuring
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary); //Switched
+
+//New Switching
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+//Destructuring Nested Arrays
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default Values - Assigning Default values will prevent getting undefined from smaller array.
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
--- destructObjects.js
+++ destructObjects.js
@@ -45,3 +45,51 @@ const restaurant = {
     );
   },
 };
+
+//Practical usage of Destructuring Objects
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
+//Object Destructuring
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+//Changing the variable names of objects
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Setting default values for objects
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating variables while destructuring (objects)
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+//Wrap into parenthesis for it to work
+//Cannot use const or let
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
