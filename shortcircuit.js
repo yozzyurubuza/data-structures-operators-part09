400d7e3482f84cca8044b9f4031b0621ab2be9b8
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-21 17:42:30 +0800

    Added codes about Nullish Coalescing Operator (??)

--- shortcircuit.js
+++ shortcircuit.js
@@ -57,6 +57,18 @@ const restaurant = {
   },
 };
 
+// The Nullish Coalescing Operator (??)
+
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (0 or '' are not falsy values)
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
+
 console.log('----- OR ------');
 
 // Use ANY data type, return ANY data type, short-circuiting
@@ -91,3 +103,4 @@ if (restaurant.orderPizza) {
 }
 
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+*/
