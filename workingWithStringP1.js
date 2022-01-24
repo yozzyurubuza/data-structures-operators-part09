89079e7b9e5f1aabf8b81104b30ba6841e9ab0f1
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-25 03:06:28 +0800

    Added codes about Working with String - Part 1

---
+++ workingWithStringP1.js
@@ -0,0 +1,126 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+//Enhanced Object Literals
+
+//You can make computations in property names
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  //Old method of getting openingHours object inside restaurant object
+  //openingHours: openingHours,
+
+  // ES6 Enhanced Object literals
+  openingHours,
+
+  // You can remove the function keyword, and it will still work
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  //Destructuring objects in Arguments in a Function right away
+  //Does not need to match the order
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${
+        (this.starterMenu[starterIndex], this.mainMenu[mainIndex])
+      } will be delivered to ${address} at ${time}`
+    );
+  },
+
+  //Old way of implementing a function
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+// Working with String - Part 1
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+//Displays characters of plane separately
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+
+//B
+console.log('B730'[0]);
+
+//Length
+console.log(airline.length);
+console.log('B737'.length);
+
+//Get letter position
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+//Slice - Extract String (start, end)
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+//Negative = Start from the end
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat :)');
+  } else {
+    console.log('You got lucky');
+  }
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+//Why you can use methods on primitive variable String
+console.log(new String('jonas')); //Shows string is an Object.
+console.log(typeof new String('jonas'));
+
+console.log(typeof new String('jonas').slice(1)); //String is automatically converted back to primitive after the method.
